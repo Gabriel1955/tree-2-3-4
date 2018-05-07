6c2c6914d0c709a2f25adbc38b4ee3f57955ca6f
Gabriel-PC\GabrielPaulino <gabrieltrilogia@gmail.com>
2018-05-06 21:38:45 -0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <conio.h>
+
+struct Node234
+{
+    int values [3];
+    struct Node234 *pointer1;
+    struct Node234 *pointer2;
+    struct Node234 *pointer3;
+    struct Node234 *pointer4;
+};
+typedef struct Node234 Node;
+
+Node createNewNode(){
+
+}
+
+void InsertValue(int value, Node *root){
+
+}
+
+void removerValue(int value, Node *root){
+
+}
+
+bool searchValue(int value, Node *root){
+
+}
+
+
+
+int main(void)
+{
+
+}
+
